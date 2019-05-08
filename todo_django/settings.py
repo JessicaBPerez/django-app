5137899c05bed4a7aa965c410ae2d548366ca783
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro-2.local>
2019-05-08 01:35:03 -0400

    Crispy form redeploy

--- todo_django/settings.py
+++ todo_django/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo',
-    'django-crispy-forms',
+    'crispy_forms',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
